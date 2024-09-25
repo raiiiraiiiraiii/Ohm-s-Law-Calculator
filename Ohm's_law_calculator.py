28839f9e0954d665fd3fd6f190dbb6a78c37b086
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-09-25 21:41:49 +0800

    Added Pseudocode (2)

--- Ohm's_law_calculator.py
+++ Ohm's_law_calculator.py
@@ -4,3 +4,10 @@
         #2.	Based on their choice, prompt the user to input the appropriate values.
         #3.	Use Ohm's Law to calculate the missing variable and display the result.
         #4.	Handle cases where division by zero might occur.
+
+
+print("Welcome to Ohm's Law Calculator!")
+
+# Ask the user what they want to calculate: Voltage, Current, or Resistance.
+# Prompt user to input values
+# Calculate the missing value and display the result
