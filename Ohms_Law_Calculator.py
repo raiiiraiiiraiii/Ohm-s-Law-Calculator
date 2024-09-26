5d65fe304e28e5937dbd9f943ea445288ef0ccad
RAILEY C. GUINTO <raileyguinto2@gmail.com>
2024-09-26 18:28:22 +0800

    Ask the user what to calculate

--- Ohms_Law_Calculator.py
+++ Ohms_Law_Calculator.py
@@ -5,9 +5,18 @@
         #3.	Use Ohm's Law to calculate the missing variable and display the result.
         #4.	Handle cases where division by zero might occur.
 
+print("Welcome to Ohm's Law Calculator!\n")
 
 # Ask the user what they want to calculate: Voltage, Current, or Resistance.
-
+while True:
+    try:
+        calculate = int(input("What would you like to calculate?\n"
+                            "1. Voltage\n"
+                            "2. Current\n"
+                            "3. Resistance\n"
+                            "Enter here: "))
+    except ValueError:
+        print("Invalid input. Enter a digit.")
 # Prompt the user to input values
 
 # Calculate
